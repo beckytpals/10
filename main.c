caff2dcbd3e65a791aa54bf14737bfc6597d06ee
beckytpals <dlk1020@sookmyung.ac.kr>
2025-11-06 16:03:56 +0900

    Update main.c

--- main.c
+++ main.c
@@ -16,19 +16,13 @@ int main(void) {
 		printf("Failed to open file!\n");
 		return -1;
 	}
-	#if 0
 	while( (c = fgetc(fp)) != EOF)
 	{
 		putchar (c);
 	}
-	#endif
+
 	
-	#if 1
-	while (fgets(input, 100, fp) > 0)
-	{
-		printf("%s", input);
-	}
-	#endif
+
 	fclose(fp);
-	return 0;
+
 }
