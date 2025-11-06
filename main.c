48e22a186f16522bd524453bc591d9206a9280a1
beckytpals <dlk1020@sookmyung.ac.kr>
2025-11-06 15:32:40 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,9 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void main(int argc, char *argv[]) {
-	char src[] = "The worst things to eat before you sleep";
-	char dst[100];
-	strcpy(dst,src);
-	printf("copied string : %s\n", dst);
+void main(void) {
+	char  str[30] = "happy C programming";
+	
+	printf("문자열 %s의 길이 : %i", str, strlen(str));
 	return 0;
 }
